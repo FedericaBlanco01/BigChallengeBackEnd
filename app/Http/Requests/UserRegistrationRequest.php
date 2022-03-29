e1ed9b08bab4e49c5ea48381b79e0184dbc531f6
Federica <federica@lightit.io>
2022-03-29 11:15:11 -0300

    register user test, controller, api route, form request

---
+++ app/Http/Requests/UserRegistrationRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRegistrationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'password' => 'required',
+            'email' => 'required|email'
+        ];
+    }
+}
---
+++ tests/Feature/RegisterUserTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class RegisterUserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_register_user()
+    {
+        $response = $this->postJson('/api/registerUser', [
+            'name'=> 'Federica',
+            'password'=> '7488.Light',
+            'email'=> 'federica@lightit.io']);
+
+        $response->assertSuccessful();
+        $this->assertDatabaseHas('users', [
+            'name'=> 'Federica',
+            'password'=> '7488.Light',
+            'email'=> 'federica@lightit.io']);
+    }
+
+}
