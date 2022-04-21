bbdc6c14c5592678c2bc21fe121a4e0b84b659ba
FedericaBlanco01 <FedericaBlanco01@users.noreply.github.com>
2022-04-21 20:25:57 +0000

    Fix styling

--- app/Http/Controllers/Files/DOSpacesController.php
+++ app/Http/Controllers/Files/DOSpacesController.php
@@ -3,18 +3,14 @@
 namespace App\Http\Controllers\Files;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\DigitalOceanDeleteRequest;
 use App\Http\Requests\DigitalOceanStoreRequest;
-use App\Http\Requests\DigitalOceanUpdateRequest;
 use App\Models\Submission;
-use App\Services\CdnService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class DOSpacesController extends Controller
 {
-    
     public function store(DigitalOceanStoreRequest $request, Submission $submission): JsonResponse
     {
         $file = $request->File('doctorProfileImageFile');
@@ -25,7 +21,8 @@ class DOSpacesController extends Controller
             "{$folder}/{$fileName}",
             file_get_contents($file)
         );
-        $submission->status= Submission::DONE_STATUS;
+        $submission->status = Submission::DONE_STATUS;
+
         return response()->json(['message' => 'File uploaded', 'name'=> $fileName], 200);
     }
 }
